1dc5a135240560ce87dcd06c7a7d1b72bf17d08f
icju <isicju@gmail.com>
2023-02-05 11:12:24 +0000

    kek

--- src/main/java/org/example/CalcUtil.java
+++ src/main/java/org/example/CalcUtil.java
@@ -1,12 +1,6 @@
 package org.example;
 
-import java.math.BigDecimal;
-import java.math.RoundingMode;
-
 public class CalcUtil {
-    public static void main(String[] args) {
-        System.out.println(divide(1, 2, 100));
-    }
 
     public static String divide(int a, int b, int precision) {
         return "";
