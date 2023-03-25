4fac779a22bb460393d715d64a18bb78ef5ec948
icju <isicju@gmail.com>
2023-03-25 11:13:10 +0000

    spring

--- src/main/java/org/example/MyServiceImpl.java
+++ src/main/java/org/example/MyServiceImpl.java
@@ -1,8 +1,6 @@
 package org.example;
 
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
