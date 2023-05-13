800b3f3bb37bcade2f4955b70e838b31f772b66e
Dima Maltsev <phoenixnety@gmail.com>
2023-05-13 19:12:02 +0300

    fix

--- src/test/java/org/example/hw12/MysqlEmbeddedTestConfig.java
+++ src/test/java/org/example/hw12/MysqlEmbeddedTestConfig.java
@@ -29,7 +29,7 @@ public class MysqlEmbeddedTestConfig {
 
     @Bean
     public EmbeddedMysql mysqlServer() {
-        EmbeddedMysql mysqlServer = anEmbeddedMysql(aMysqldConfig(v5_6_21)
+        EmbeddedMysql mysqlServer = anEmbeddedMysql(aMysqldConfig(v5_7_latest)
                 .withPort(mysqlPort)
                 .withUser("test", "test")
                 .build())
--- src/test/resources/init_db.sql
+++ src/test/resources/init_db.sql
@@ -26,7 +26,7 @@ INSERT INTO books(book_name, book_description, book_publish_date, author_id)
 VALUES(
           'Martin Eden',
           'Living in Oakland at the beginning of the 20th century, Martin Eden struggles to rise above his destitute, proletarian circumstances through an intense and passionate pursuit of self-education, hoping to achieve a place among the literary elite.',
-          STR_TO_DATE('1909', '%YYYY'),
+          STR_TO_DATE('01,01,1799', '%d,%m,%Y'),
           777
       );
 
@@ -34,7 +34,7 @@ INSERT INTO books(book_name, book_description, book_publish_date, author_id)
 VALUES(
           'One Hundred Years of Solitude',
           'Is the story of seven generations of the Buendía Family in the town of Macondo. The founding patriarch of Macondo, José Arcadio Buendía, and Úrsula Iguarán, his wife (and first cousin), leave their hometown in Riohacha, Colombia, after José Arcadio kills Prudencio Aguilar after a cockfight for suggesting José Arcadio was impotent.',
-          STR_TO_DATE('1967', '%YYYY'),
+          STR_TO_DATE('01,01,1799', '%d,%m,%Y'),
           777
       );
 
@@ -42,21 +42,21 @@ INSERT INTO authors
 VALUES(
           555,
           'PushkinFromMysqlEmbedded',
-          STR_TO_DATE('06-JUN-1799', '%d-%M-%Y')
+          STR_TO_DATE('06,June,1799', '%d,%M,%Y')
       );
 
 INSERT INTO authors
 VALUES(
           666,
           'PushkinFromMysqlEmbedded',
-          STR_TO_DATE('06-JUN-1799', '%d-%M-%Y')
+          STR_TO_DATE('06,June,1799', '%d,%M,%Y')
       );
 
 INSERT INTO authors
 VALUES(
           777,
           'PushkinFromMysqlEmbedded',
-          STR_TO_DATE('06-JUN-1799', '%d-%M-%Y')
+          STR_TO_DATE('06,June,1799', '%d,%M,%Y')
       );
 
 
